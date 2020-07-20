032b202cdddf89deddf8f9b2f027739ed2a02dc5
Prabhash96isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-07-20 20:20:15 +0530

    react

---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Hero extends Component {
+
+    state = {heroId: 99};
+    render(){
+        return(
+        <React.Fragment>
+            <h1>Avengers incoming..........</h1>
+        <button>Click Here {this.state.heroId}</button>
+        </React.Fragment>
+        );
+    }
+}
+
+export default Hero;
